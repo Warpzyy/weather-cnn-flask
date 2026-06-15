81e5b143d9bb28969a26b50ea4d0988493691432
Warpzyy <whiffasm18@gmail.com>
2026-06-15 13:15:31 +0700

    Fix gdown download method

--- app.py
+++ app.py
@@ -24,8 +24,12 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 if not os.path.exists(MODEL_PATH):
     print("⏳ Downloading model dari Google Drive...")
-    url = f'https://drive.google.com/uc?id={GDRIVE_FILE_ID}'
-    gdown.download(url, MODEL_PATH, quiet=False)
+    gdown.download(
+        id=GDRIVE_FILE_ID,
+        output=MODEL_PATH,
+        quiet=False,
+        fuzzy=True
+    )
     print("✅ Model berhasil didownload!")
 
 print("⏳ Loading model...")
